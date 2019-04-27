362754d52c4ed02cb86acf6f5a5812981281153d
EzraIII <Ezra3Ezra3@gmail.com>
2019-04-26 22:56:32 -0700

    Added Credits button and scene. Closes #11.

--- src/LDJam44/Assets/Scripts/SceneNames.cs
+++ src/LDJam44/Assets/Scripts/SceneNames.cs
@@ -3,4 +3,5 @@ public static class SceneNames {
     public static string GameScene = "theGame";
     public static string WinScene = "WinScreen";
     public static string LoseScene = "LoseScreen";
+    public static string CreditsScene = "CreditsScreen";
 }
