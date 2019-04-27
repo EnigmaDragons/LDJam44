29f25d94391350978eda857ed25433ce454fab93
noah reinagel <noah.r9@gmail.com>
2019-04-27 00:45:06 -0700

    Now you can win

--- src/LDJam44/Assets/Scripts/Navigation.cs
+++ src/LDJam44/Assets/Scripts/Navigation.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Navigation : MonoBehaviour
 {
     public void NavigateToTitleScene() => SceneManager.LoadScene(SceneNames.TitleScene);
-    public void NavigateToGameScene() => SceneManager.LoadScene(SceneNames.GameScene);
+    public void NavigateToGameScene() => SceneManager.LoadScene(SceneNames.SpaceStationScene);
     public void NavigateToWinScene() => SceneManager.LoadScene(SceneNames.WinScene);
     public void NavigateToLoseScene() => SceneManager.LoadScene(SceneNames.LoseScene);
     public void NavigateToCreditsScene() => SceneManager.LoadScene(SceneNames.CreditsScene);
