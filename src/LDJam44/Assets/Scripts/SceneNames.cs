c7fe516de969f9b10ff7dfb7af289b72a855bc03
EzraIII <Ezra3Ezra3@gmail.com>
2019-04-27 00:39:42 -0700

    Adding Starting Logo Scene. Closes #49.

--- src/LDJam44/Assets/Scripts/SceneNames.cs
+++ src/LDJam44/Assets/Scripts/SceneNames.cs
@@ -4,4 +4,5 @@ public static class SceneNames {
     public static string WinScene = "WinScreen";
     public static string LoseScene = "LoseScreen";
     public static string CreditsScene = "CreditsScreen";
+    public static string StartingLogoScene = "StartingLogoScene";
 }
