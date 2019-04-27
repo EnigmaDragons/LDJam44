302f34390185f585d74671d6b26c2b600a90486f
GC54 <sahilpohare>
2019-04-27 12:44:14 +0530

    Enemy

---
+++ src/LDJam44/Assets/Sahil/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public virtual void Attack(Vector3 target) { }
+    public virtual void Move(Vector3 location,float moveSpeed) { }
+}
+
+
