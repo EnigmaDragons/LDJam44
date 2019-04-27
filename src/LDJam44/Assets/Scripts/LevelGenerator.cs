a8936e3cc726007a13603211edd021816e1b24be
SilasReinagel <silas.reinagel@gmail.com>
2019-04-27 14:29:50 -0700

    Added Level Generation. Closes #85

---
+++ src/LDJam44/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    // Settings
+    [SerializeField] float travelDistance = 95f;
+    [SerializeField] int difficulty = 3;
+
+    // Object Prototypes
+    [SerializeField] GameObject levelEnd;
+    [SerializeField] GameObject spaceStation;
+
+    void Start()
+    {
+        Instantiate(levelEnd, new Vector3(0, 0, travelDistance), Quaternion.identity);
+        Instantiate(spaceStation, new Vector3(0, 0, travelDistance + 5f), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
