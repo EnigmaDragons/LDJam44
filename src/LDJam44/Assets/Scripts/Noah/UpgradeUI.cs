c62ecfc503a7303dd0fc54c008c5c50716732e98
noah reinagel <noah.r9@gmail.com>
2019-04-29 00:16:01 -0700

    built upgrades into space station

---
+++ src/LDJam44/Assets/Scripts/Noah/UpgradeUI.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts.Noah;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeUI : MonoBehaviour
+{
+    private MutablePlayer player;
+    private bool isMaxed;
+    private int cost;
+
+    public UpgradeRules Upgrade;
+    public Text Level;
+    public Text Name;
+    public Text Description;
+    public Text Cost;
+    public Button BuyUpgrade;
+    
+    void Start()
+    {
+        player = GameObject.Find("GameState").GetComponent<GameState>().PlayerData;
+        isMaxed = player.UpgradeLevel(Upgrade.Name) == Upgrade.Costs.Length;
+        cost = isMaxed ? 0 : Upgrade.Costs[player.UpgradeLevel(Upgrade.Name)];
+        Level.text = $"{player.UpgradeLevel(Upgrade.Name)}/{Upgrade.Costs.Length}";
+        Name.text = Upgrade.Name;
+        Description.text = Upgrade.Description;
+        Cost.text = isMaxed ? "MAXED" : $"{cost} LF";
+        Update();
+    }
+
+    void Update()
+    {
+        var canBuy = !isMaxed && player.LifeForce > cost;
+        BuyUpgrade.gameObject.SetActive(canBuy);
+    }
+
+    public void Buy()
+    {
+        player.LifeForce -= cost;
+        player.Upgrades[Upgrade.Name]++;
+        Start();
+    }
+}
