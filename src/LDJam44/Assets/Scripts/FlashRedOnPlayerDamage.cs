0fa0bdb9a3f58a28dbcca688cf8fd9ab7e971853
SilasReinagel <silas.reinagel@gmail.com>
2019-04-28 18:10:42 -0700

    Screen flash and sound on Player Hit. Closes #157. Closes #159

---
+++ src/LDJam44/Assets/Scripts/FlashRedOnPlayerDamage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+class FlashRedOnPlayerDamage : VerboseMonoBehaviour
+{
+    public Health Health;
+
+    private Image image;
+    private Color targetColor;
+    private Color targetTransparent;
+
+    bool wasFreshlyDamaged;
+
+    void Start()
+    {
+        image = VerboseGetComponent<Image>();
+        targetColor = new Color(image.color.r, image.color.g, image.color.b, 1f);
+        targetTransparent = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        Health.OnDamage(() => wasFreshlyDamaged = true);
+    }
+
+    void Update()
+    {
+        if (wasFreshlyDamaged)
+            image.color = Color.Lerp(image.color, targetColor, 20 * Time.deltaTime);
+        else
+            image.color = Color.Lerp(image.color, targetTransparent, 20 * Time.deltaTime);
+
+        if (image.color.a >= 0.6)
+            wasFreshlyDamaged = false;
+    }
+}
