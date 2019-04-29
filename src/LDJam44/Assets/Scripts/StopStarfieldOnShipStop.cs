9159cf2ff1070ee9791d62fccb9f546a90b6a080
SilasReinagel <silas.reinagel@gmail.com>
2019-04-29 01:54:33 -0700

    Starfield stops on Ship done. Closes #118

---
+++ src/LDJam44/Assets/Scripts/StopStarfieldOnShipStop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+class StopStarfieldOnShipStop : VerboseMonoBehaviour
+{
+    private ParticleSystem particles;
+    private Ship ship;
+
+    private void Start()
+    {
+        ship = VerboseFindObjectOfType<Ship>();
+        particles = VerboseGetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (ship.Stopped)
+            Destroy(gameObject);
+    }
+}
