d84fc5646cbc26512ea24802fab692bd429b8f78
noah reinagel <noah.r9@gmail.com>
2019-04-29 02:50:43 -0700

    made the sweet victory and defeat screen

---
+++ src/LDJam44/Assets/DefeatAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefeatAnimation : MonoBehaviour
+{
+    private GameServices game;
+    public AudioClip ExplosionSound;
+    public GameObject Spaceship;
+    public GameObject Explosion;
+    public GameObject Debris;
+
+    void Start()
+    {
+        game = FindObjectOfType<GameServices>();
+        StartCoroutine(Animation());
+    }
+
+    private IEnumerator Animation()
+    {
+        yield return new WaitForSeconds(2);
+        Explosion.SetActive(true);
+        game.PlaySoundEffect(ExplosionSound);
+        yield return new WaitForSeconds(0.1f);
+        Spaceship.SetActive(false);
+        Debris.SetActive(true);
+    }
+}
