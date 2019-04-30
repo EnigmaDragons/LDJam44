3cbd78231a437896133247f16ce8e0b0f5e0a310
SilasReinagel <silas.reinagel@gmail.com>
2019-04-29 17:03:01 -0700

    Added Credit Texts.

--- src/LDJam44/Assets/Scripts/Ezra3/RoleAndNameScript.cs
+++ src/LDJam44/Assets/Scripts/Ezra3/RoleAndNameScript.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class RoleAndNameScript : MonoBehaviour
 {
-
-
     public Text nameText;
     public Text roleText;
     int x = 0;
@@ -37,6 +33,26 @@ public class RoleAndNameScript : MonoBehaviour
         }
     }
 
+    public static CreditData ProducedBy = new CreditData
+    {
+        //RoleColor = Color.
+        Role = "Produced By",
+        Name = "Enigma Dragons"
+    };
+
+    public static CreditData Director = new CreditData
+    {
+        //RoleColor = Color.
+        Role = "Game Director",
+        Name = "Silas Reinagel"
+    };
+
+    public static CreditData ProjectManager = new CreditData
+    {
+        //RoleColor = Color.
+        Role = "Project Manager",
+        Name = "Silas Reinagel"
+    };
 
     public static CreditData LeadProgrammer = new CreditData
     {
@@ -44,37 +60,86 @@ public class RoleAndNameScript : MonoBehaviour
         Role = "Lead Programmer",
         Name = "Silas Reinagel"
     };
+
     public static CreditData LeadDesigner = new CreditData
     {
         //RoleColor = Color.
         Role = "Lead Designer",
         Name = "Noah Reinagel"
     };
+
     public static CreditData EzraProgrammer = new CreditData
     {
         //RoleColor = Color.
         Role = "Programmer",
         Name = "Ezra3"
     };
+
+    public static CreditData Animator = new CreditData
+    {
+        //RoleColor = Color.
+        Role = "Animations",
+        Name = "Ezra3"
+    };
+
+    public static CreditData UI = new CreditData
+    {
+        Role = "User Interface",
+        Name = "Noah Reinagel"
+    };
+
     public static CreditData LeadArtist = new CreditData
     {
         //RoleColor = Color.
         Role = "Lead Artist",
-        Name = "Shadow"
+        Name = "Shaddo"
     };
-    public static CreditData LeadMusician = new CreditData
+    public static CreditData SoundEffects = new CreditData
     {
         //RoleColor = Color.
-        Role = "Lead Musician",
+        Role = "Sound Effects",
         Name = "Bradley Seymour"
     };
-    public static CreditData LeadStorymaker = new CreditData
+    public static CreditData MusicDirection = new CreditData
+    {
+        //RoleColor = Color.
+        Role = "Music Direction",
+        Name = "Silas Reinagel"
+    };
+
+    public static CreditData SettingAndStory = new CreditData
     {
         //RoleColor = Color.
         Role = "Lead Storymaker",
-        Name = "Playfulwarrior"
+        Name = "Cynthia Reinagel"
     };
-    public static CreditData[] AllCredits = { LeadProgrammer, LeadDesigner, EzraProgrammer, LeadArtist, LeadMusician, LeadStorymaker};
+
+    public static CreditData Tester = new CreditData
+    {
+        //RoleColor = Color.
+        Role = "Quality Assurance",
+        Name = "Gordy Keene"
+    };
+
+    public static CreditData[] AllCredits = 
+    {
+        ProducedBy,
+
+        Director,
+        LeadDesigner,
+        LeadArtist,
+
+        LeadProgrammer,
+        EzraProgrammer,
+        UI,
+
+        Tester,
+        MusicDirection,
+        ProjectManager,
+        SoundEffects,
+        SettingAndStory,
+        Animator
+    };
 }
 
 public class CreditData
